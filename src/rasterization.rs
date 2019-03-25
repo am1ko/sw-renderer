b5095ba1379afadc859423d279b8fcb5f5948346
Mikko Ala-Louko <mikko.alalouko@gmail.com>
2019-03-25 19:32:39 +0200

    some issue still

--- src/rasterization.rs
+++ src/rasterization.rs
@@ -1,6 +1,17 @@
 use core::{Color, DisplayBuffer, LineSegment, Renderable, Triangle};
 use na::{Vector3};
 
+/// Interpolate a 1D function
+///
+/// * 'x' X-coordinate at which to find the interpolated Y-value
+/// * 'y1' Y-value at x=x1
+/// * 'y0' Y-value at x=x0
+/// * 'x0' Lower bound of X
+/// * 'x1' Upper bound of X
+fn interpolate(x: f32, y1: f32, y0: f32, x1: f32, x0: f32) -> f32 {
+    y0 + (x - x0)*(y1 - y0)/(x1-x0)
+}
+
 impl Renderable for Triangle<Vector3<f32>> {
     /// Draw a color-filled triangle
     fn render(&self, color: Color, buffer: &mut DisplayBuffer) {
@@ -92,19 +103,28 @@ fn fill_bottom_flat_triangle(
     color: Color,
     buffer: &mut DisplayBuffer,
 ) {
+    // Top vertex
     let v0 = triangle.to_usize().v0;
-    let v1 = triangle.to_usize().v1;
-    let v2 = triangle.to_usize().v2;
+    // Bottom left vertex
+    let v1 = if triangle.v1.x < triangle.v2.x { triangle.to_usize().v1 } else {triangle.to_usize().v2};
+    // Bottom right vertex
+    let v2 = if triangle.v1.x < triangle.v2.x { triangle.to_usize().v2 } else {triangle.to_usize().v1};
+
+    // dx / dy for both edges of the triangle
     let inv_slope_1 = (v0.x as f32 - v1.x as f32) / (v1.y as f32 - v0.y as f32);
     let inv_slope_2 = (v0.x as f32 - v2.x as f32) / (v1.y as f32 - v0.y as f32);
 
-    let mut curr_x_1: f32 = v0.x as f32;
-    let mut curr_x_2: f32 = v0.x as f32;
+    // Scan line starting x-coordinate
+    let mut x_start: f32 = v0.x as f32;
+    // Scan line ending x-coordinate
+    let mut x_end: f32 = v0.x as f32;
 
-    for y in (v1.y..v0.y + 1).rev() {
-        // todo: interpolate z for start and end
-        let scan_line_start = Vector3::new(curr_x_1, y as f32, triangle.v0.z);
-        let scan_line_end = Vector3::new(curr_x_2, y as f32, triangle.v0.z);
+    for y in (v1.y..=v0.y).rev() {
+        // Interpolate the z-coordinate between two vertices of the triangle
+        let z_start = interpolate(y as f32, v0.z as f32, v1.z as f32, v0.y as f32, v1.y as f32);
+        let z_end = interpolate(y as f32, v0.z as f32, v2.z as f32, v0.y as f32, v2.y as f32);
+        let scan_line_start = Vector3::new(x_start, y as f32, z_start);
+        let scan_line_end = Vector3::new(x_end, y as f32, z_end);
 
         let scan_line = LineSegment {
             v0: scan_line_start,
@@ -113,8 +133,8 @@ fn fill_bottom_flat_triangle(
 
         scan_line.render(color, buffer);
 
-        curr_x_1 += inv_slope_1;
-        curr_x_2 += inv_slope_2;
+        x_start += inv_slope_1;
+        x_end += inv_slope_2;
     }
 }
 
@@ -128,18 +148,26 @@ fn fill_top_flat_triangle(
     color: Color,
     buffer: &mut DisplayBuffer,
 ) {
-    let v0 = triangle.to_usize().v0;
-    let v1 = triangle.to_usize().v1;
+    // Top left vertex
+    let v0 = if triangle.v0.x < triangle.v1.x { triangle.to_usize().v0 } else {triangle.to_usize().v1};
+    // Top right vertex
+    let v1 = if triangle.v0.x < triangle.v1.x { triangle.to_usize().v1 } else {triangle.to_usize().v0};
+    // Bottom vertex
     let v2 = triangle.to_usize().v2;
+
+    // dx / dy for both edges of the triangle
     let inv_slope_1 = (v0.x as f32 - v2.x as f32) / (v2.y as f32 - v0.y as f32);
     let inv_slope_2 = (v1.x as f32 - v2.x as f32) / (v2.y as f32 - v1.y as f32);
 
-    let mut curr_x_1 = v2.x as f32;
-    let mut curr_x_2 = v2.x as f32;
+    let mut x_start = v2.x as f32;
+    let mut x_end = v2.x as f32;
 
-    for y in v2.y..v0.y + 1 {
-        let scan_line_start = Vector3::new(curr_x_1, y as f32, triangle.v0.z);
-        let scan_line_end = Vector3::new(curr_x_2, y as f32, triangle.v0.z);
+    // Loop y from bottom to top of the triangle
+    for y in v2.y..=v0.y {
+        let z_start = interpolate(y as f32, v0.z as f32, v2.z as f32, v0.y as f32, v2.y as f32);
+        let z_end = interpolate(y as f32, v1.z as f32, v2.z as f32, v1.y as f32, v2.y as f32);
+        let scan_line_start = Vector3::new(x_start, y as f32, z_start);
+        let scan_line_end = Vector3::new(x_end, y as f32, z_end);
 
         let scan_line = LineSegment {
             v0: scan_line_start,
@@ -148,7 +176,21 @@ fn fill_top_flat_triangle(
 
         scan_line.render(color, buffer);
 
-        curr_x_1 -= inv_slope_1;
-        curr_x_2 -= inv_slope_2;
+        x_start -= inv_slope_1;
+        x_end -= inv_slope_2;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpolate() {
+        // Case: Interpolate y = 0.5x at x: 1.0
+        // Expect: Y-value close to 0.5
+        let y = interpolate(1.0, 1.0, 0.0, 2.0, 0.0);
+        assert!(y > 0.495);
+        assert!(y < 0.505);
     }
 }
